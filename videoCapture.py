6eed17b9ddcceb1bcd8039811b72d50bce7e7c1b
Master_Pew <58269281+pewpeww39@users.noreply.github.com>
2023-01-25 21:48:24 -0500

    Update videoCapture.py

--- videoCapture.py
+++ videoCapture.py
@@ -2,14 +2,16 @@ import os
 import RPi.GPIO as GPIO
 
 
-inputPin = 26
-outputPin = 19
+inputPin = 6
+outputPin = 5
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(inputPin, GPIO.IN)
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(outputPin, GPIO.OUT)
+GPIO.setup(inputPin, GPIO.IN, initial=1)
+
 while(1):
-	if inputPin == 1:
-		os.system("libcamera-vid -t 60000 -o firstFlight.h264")
-		outputPin = 1
+        if GPIO.input(inputPin) == 1:
+                os.system("libcamera-vid -t 14000 -o test.h264 -b 10000000 --wi>
+                outputPin = 1
+                break;
 
