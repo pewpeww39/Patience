d7d664d4fd7881957a7ee0cfd1c485ef9375820d
jacobpew <jacobpew@outlook.com>
2023-01-26 22:32:35 -0500

    update

--- videoCapture.py
+++ videoCapture.py
@@ -7,10 +7,10 @@ outputPin = 5
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(outputPin, GPIO.OUT)
-GPIO.setup(inputPin, GPIO.IN, initial=1)
+GPIO.setup(inputPin, GPIO.IN)
 
 while(1):
 	if GPIO.input(inputPin) == 1:
-		os.system("libcamera-vid -t 14000 -o test.h264 -b 10000000 --width 1920 --height 1000 --intra 30")
+		os.system("libcamera-vid -t 14000 -o ~/Video/firstFlight.h264 -b 10000000 --width 1920 --height 1000 --intra 30")
 		outputPin = 1
 		break;
